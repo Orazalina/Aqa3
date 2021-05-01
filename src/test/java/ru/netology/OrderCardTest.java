aa3e4d8668c00834b99f6cbc8f4a06a46f13e71a
Anara Orazalina <orazalina.anara@inbox.ru>
2021-05-02 01:33:45 +0600

    tests added and CI corrected

--- src/test/java/ru/netology/OrderCardTest.java
+++ src/test/java/ru/netology/OrderCardTest.java
@@ -15,7 +15,7 @@ public class OrderCardTest {
 
     @BeforeAll
     static void setUpAll() {
-            //System.setProperty("webdriver.gecko.driver", "./driver/firefox/geckodriver.exe");
+        //System.setProperty("webdriver.gecko.driver", "./driver/firefox/geckodriver.exe");
         WebDriverManager.firefoxdriver().setup();
 
     }
@@ -23,26 +23,18 @@ public class OrderCardTest {
     @BeforeEach
     void setUp() {
         FirefoxOptions options = new FirefoxOptions();
-        //options.addArguments("--marionette");
-        //options.addArguments("--marionette");
-        //options.addArguments("--disable-dev-shm-usage");
-        //options.addArguments("--no-sandbox");
-        //options.addArguments("--headless");
-        //options.addArguments("-foreground");
-        //options.addArguments("-no-remote");
-        //options.addArguments("-profile");
-        //options.setHeadless(true);
+        options.setHeadless(true);
         driver = new FirefoxDriver(options);
     }
 
     @AfterEach
-    void  tearDown() {
+    void tearDown() {
         driver.quit();
         driver = null;
     }
 
     @Test
-    void shouldTest() {
+    void correctFormCompletion() {
         driver.get("http://localhost:9999");
         List<WebElement> textFields = driver.findElements(By.className("input__control"));
         textFields.get(0).sendKeys("Иванова Анна");
@@ -54,6 +46,30 @@ public class OrderCardTest {
         Assertions.assertEquals(expectedMessage.trim(), actualMessage.trim());
     }
 
+    @Test
+    void withLatinLetters() {
+        driver.get("http://localhost:9999");
+        List<WebElement> textFields = driver.findElements(By.className("input__control"));
+        textFields.get(0).sendKeys("Ivanova Anna");
+        textFields.get(1).sendKeys("+77777777777");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+        String actualMessage = driver.findElement(By.className("input__sub")).getText();
+        String expectedMessage = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        Assertions.assertEquals(expectedMessage.trim(), actualMessage.trim());
+    }
 
-
+    @Test
+    void withIncorrectNumber() {
+        driver.get("http://localhost:9999");
+        List<WebElement> textFields = driver.findElements(By.className("input__control"));
+        textFields.get(0).sendKeys("Иванова Анна");
+        textFields.get(1).sendKeys("+77777777");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+        textFields = driver.findElements(By.className("input__sub"));
+        String message = textFields.get(1).getText();
+        String expectedMessage = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        Assertions.assertEquals(expectedMessage.trim(), message.trim());
+    }
 }

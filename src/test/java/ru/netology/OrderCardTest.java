018006f072e0b316950ec97e37f6af778f75a9d0
Anara Orazalina <orazalina.anara@inbox.ru>
2021-05-02 20:31:58 +0600

    tests added

--- src/test/java/ru/netology/OrderCardTest.java
+++ src/test/java/ru/netology/OrderCardTest.java
@@ -43,7 +43,7 @@ public class OrderCardTest {
         driver.findElement(By.tagName("button")).click();
         String actualMessage = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         String expectedMessage = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        Assertions.assertEquals(expectedMessage.trim(), actualMessage.trim());
+        Assertions.assertEquals(expectedMessage, actualMessage.trim());
     }
 
     @Test
@@ -56,7 +56,7 @@ public class OrderCardTest {
         driver.findElement(By.tagName("button")).click();
         String actualMessage = driver.findElement(By.className("input__sub")).getText();
         String expectedMessage = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
-        Assertions.assertEquals(expectedMessage.trim(), actualMessage.trim());
+        Assertions.assertEquals(expectedMessage, actualMessage.trim());
     }
 
     @Test
@@ -70,21 +70,47 @@ public class OrderCardTest {
         textFields = driver.findElements(By.className("input__sub"));
         String message = textFields.get(1).getText();
         String expectedMessage = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
-        Assertions.assertEquals(expectedMessage.trim(), message.trim());
+        Assertions.assertEquals(expectedMessage, message.trim());
     }
 
     @Test
     void withNoName() {
         driver.get("http://localhost:9999");
         List<WebElement> textFields = driver.findElements(By.className("input__control"));
-        textFields.get(1).sendKeys("+77777777");
+        textFields.get(0);
+        textFields.get(1).sendKeys("+77777777777");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
         textFields = driver.findElements(By.className("input__sub"));
         String message = textFields.get(0).getText();
         String expectedMessage = "Поле обязательно для заполнения";
-        Assertions.assertEquals(expectedMessage.trim(), message.trim());
+        Assertions.assertEquals(expectedMessage, message.trim());
     }
 
+    @Test
+    void withNoTel() {
+        driver.get("http://localhost:9999");
+        List<WebElement> textFields = driver.findElements(By.className("input__control"));
+        textFields.get(0).sendKeys("Иванова Анна");
+        textFields.get(1);
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.tagName("button")).click();
+        textFields = driver.findElements(By.className("input__sub"));
+        String message = textFields.get(1).getText();
+        String expectedMessage = "Поле обязательно для заполнения";
+        Assertions.assertEquals(expectedMessage, message.trim());
+    }
 
+    @Test
+    void withNoClickCheckbox() {
+        driver.get("http://localhost:9999");
+        List<WebElement> textFields = driver.findElements(By.className("input__control"));
+        textFields.get(0).sendKeys("Иванова Анна");
+        textFields.get(1).sendKeys("+77777777777");
+        driver.findElement(By.className("checkbox__box"));
+        driver.findElement(By.tagName("button")).click();
+        String actualMessage = driver.findElement(By.cssSelector("[data-test-id=agreement]")).getText();
+        String expectedMessage = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
+        Assertions.assertEquals(expectedMessage, actualMessage.trim());
+    }
 }
